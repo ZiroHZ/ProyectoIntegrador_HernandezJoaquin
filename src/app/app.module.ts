d5aaeacf5c8471f5b11fbe70785f53749b1331d9
ZiroHZ <109875733+ZiroHZ@users.noreply.github.com>
2022-07-31 15:14:46 -0300

    Header
    Se crea el header con logos de SOCIAL y Banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
