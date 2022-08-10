787d77aacdec9925f79627332a58a3adb6023ad0
ZiroHZ <109875733+ZiroHZ@users.noreply.github.com>
2022-08-10 11:44:45 -0300

    cambio nombre a carpeta
    Le cambie el nombre sin espacios para que no haya errores

---
+++ BackEnd/src/main/java/com/portfolio/joaquinh/JoaquinhApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.joaquinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaquinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaquinhApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/joaquinh/JoaquinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.joaquinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaquinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
